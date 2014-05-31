3ecf545a9726e738ada7f42283898c6cb7ffb8af
Dennis Dixon <ddixonjr@appivot.com>
2014-05-31 15:54:53 -0500

    Project start files

---
+++ Flickrmals/DDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DDAppDelegate.h
+//  Flickrmals
+//
+//  Created by Dennis Dixon on 5/31/14.
+//  Copyright (c) 2014 Appivot LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Flickrmals/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Flickrmals
+//
+//  Created by Dennis Dixon on 5/31/14.
+//  Copyright (c) 2014 Appivot LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DDAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DDAppDelegate class]));
+    }
+}
